dd0b643c38d2e86280e2913c7d423c442cf1b227
Anon <No email>
2021-03-20 15:08:48 +0800

    Tune the build script for my setup

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-Defconfig_Folder=arch/arm64/configs
-Kernel_Root=~/android/build/kernel/asus/sm8250
-Android_Build=~/android/build/omni/android-10
-Clang_Google=prebuilts/clang/host
-Prebuilt_Clang=clang-r383902c
-GCC_Google_Arm64=prebuilts/gcc/linux-x86/aarch64
-Kernel_Output_Path=out/arch/arm64/boot
+defconfig_folder=arch/arm64/configs
+kernel_root=/mnt/e/Phone_Zf7Stuff/Kernel/StockKernel/kernel/msm-4.19
+clang_path=/mnt/e/Phone_Zf7Stuff/Kernel/clang-r383902c
+arm64_gcc_path=/mnt/e/Phone_Zf7Stuff/Kernel/aarch64-linux-android-4.9
+kernel_output_path=out/arch/arm64/boot
 
 echo
 echo "Clean Build Directory?"
@@ -32,17 +30,17 @@ echo
 mkdir -p out
 export ARCH=arm64
 export SUBARCH=arm64
-export CLANG_PATH=$Android_Build/$Clang_Google/linux-x86/$Prebuilt_Clang/bin
+export CLANG_PATH=$clang_path/bin
 export PATH=${CLANG_PATH}:${PATH}
-export DTC_EXT=$Kernel_Root/dtc-aosp
+export DTC_EXT=$kernel_root/dtc-aosp
 export CLANG_TRIPLE=aarch64-linux-gnu-
-export CROSS_COMPILE=$Android_Build/$GCC_Google_Arm64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=$arm64_gcc_path/bin/aarch64-linux-android-
 
 echo
 echo "Choose DEFCONFIG"
 echo 
 
-DEFCONFIG=($(find $Defconfig_Folder -type f -exec echo '{}' \; | awk -F'configs/' '{print $NF}'))
+DEFCONFIG=($(find $defconfig_folder -type f -exec echo '{}' \; | awk -F'configs/' '{print $NF}'))
 select choice in "${DEFCONFIG[@]}"; do
     make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip O=out "$choice"
 	break
@@ -54,15 +52,15 @@ echo
 
 time make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip O=out -j$(($(nproc) * 2))
 
-if [ -e $Kernel_Output_Path/Image ]; then
+if [ -e $kernel_output_path/Image ]; then
 	echo
 	echo "Compress Kernel Image"
 	echo 
-	gzip -9 -k -f $Kernel_Output_Path/Image
+	gzip -9 -k -f $kernel_output_path/Image
 	echo
 	echo "Compile DTBs"
 	echo
-	find $Kernel_Output_Path/dts -name '*.dtb' -exec cat {} + > $Kernel_Output_Path/dtb.img
+	find $kernel_output_path/dts -name '*.dtb' -exec cat {} + > $kernel_output_path/dtb.img
 	echo
 	echo "Build Complete!"
 	echo
